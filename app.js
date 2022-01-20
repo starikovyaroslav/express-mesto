ac27eda532383fe0b63d07f14f7f4f678a0ad726
starikovyaroslav <slava.com.com90@gmail.com>
2022-01-20 20:49:41 +0700

    feat: CORS added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const { userValidation, loginValidation } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
@@ -12,9 +13,36 @@ const NotFoundError = require('./errors/NotFoundError');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const allowedCors = [
+  'https://mesto.frontend.sidorov.nomoredomains.monster',
+  'https://api.mesto.sidorov.nomoredomains.monster',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(requestLogger);
 
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
@@ -30,6 +58,7 @@ app.use('*', () => {
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
