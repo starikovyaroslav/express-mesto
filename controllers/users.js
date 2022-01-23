d44ec443751773b19d395d29c99f62493193eeb7
starikovyaroslav <slava.com.com90@gmail.com>
2022-01-23 15:15:24 +0700

    feat: добавлен token

--- controllers/users.js
+++ controllers/users.js
@@ -111,7 +111,7 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       })
-        .send('Авторизация успешна!');
+        .send({ token });
     })
     .catch(next);
 };
